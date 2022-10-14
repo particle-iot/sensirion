f62a50d59ade2b85fc6a7dfb3390d5663bf6dfb5
James Wilson <james.wilson@particle.io>
2022-10-14 13:12:10 -0700

    Update examples

--- examples/sht/usage.cpp
+++ examples/sht/usage.cpp
@@ -1,60 +1,60 @@
 #include "Particle.h"
 #include "Sht3x.h"
 
-//#define USE_SINGLE_SHOT
-#define USE_PERIODIC_DATA
+#define USE_SINGLE_SHOT
 
-SerialLogHandler logHandler(115200, LOG_LEVEL_ALL, {
+SerialLogHandler logHandler(
+  115200,
+  LOG_LEVEL_ALL,
+  {
     // { "app", LOG_LEVEL_ALL },
     // { "hal", LOG_LEVEL_NONE},
     // { "lwip", LOG_LEVEL_NONE},
-});
-
-STARTUP(
-    System.enableFeature(FEATURE_RESET_INFO);
-    System.enableFeature(FEATURE_RETAINED_MEMORY);
+  }
 );
+
+STARTUP(System.enableFeature(FEATURE_RESET_INFO);
+        System.enableFeature(FEATURE_RETAINED_MEMORY););
 
 SYSTEM_THREAD(ENABLED);
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
-Sht3x* sht;
+Sht3x sht(Wire, Sht3x::ADDR_A, 255);
 
-void setup() {
+void setup()
+{
     Wire.setSpeed(CLOCK_SPEED_100KHZ);
-    sht  = new (std::nothrow) Sht3x(Wire, Sht3x::Addr::ADDR_A, 0);
-    sht->init();
+    Wire.begin();
+    if (!sht.init()) {
+        Log.error("failed to initialize device, halting");
+        while (true) {}
+    }
 
-#if defined(USE_PERIODIC_DATA)
-    sht->measure(Sht3x::Mode::PERIODIC_DATA, Sht3x::SingleMode::SINGLE_NONE, Sht3x::PeriodicMode::HIGH_10_MPS);
-#endif//defined(USE_PERIODIC_DATA)
+#if !defined(USE_SINGLE_SHOT)
+    sht.startPeriodicMeasurement(Sht3x::PeriodicMode::HIGH_10_MPS);
+#endif // !defined(USE_SINGLE_SHOT)
 }
 
-void loop() {
+void loop()
+{
     static uint32_t read_time_ms = millis();
-    
-    if(millis() - read_time_ms >= 1000) {
+
+    if (millis() - read_time_ms >= 1000) {
+        float temp;
+        float humidity;
 #if defined(USE_SINGLE_SHOT)
-        float temp = 0;
-        float humidity = 0;
-        if(sht->singleShotMeasureAndRead(temp, humidity) == 
-                SensirionBase::ErrorCodes::NO_ERROR) {
-            Log.info("temp:%f  humidity:%f", temp, humidity);
-        }
-        else {
-            Log.info("failed to read temp and humidity");
+        if (sht.singleMeasurement(temp, humidity)) {
+            Log.info("temp:%f, humidity:%f", temp, humidity);
+        } else {
+            Log.error("failed to read temp and humidity");
         }
 #else
-        Vector<float> values {};
-        if(sht->periodicDataRead(values) == SensirionBase::ErrorCodes::NO_ERROR) {
-            for(auto iter : values) {
-                Log.info("values:%f", iter);
-            }
+        if (sht.periodicDataRead(temp, humidity)) {
+            Log.info("temp:%f, humidity:%f", temp, humidity);
+        } else {
+            Log.error("failed to read periodic temp and humidity");
         }
-        else {
-            Log.info("failed to read periodic temp and humidity");
-        }
-#endif //defined(USE_SINGLE_SHOT)
+#endif // defined(USE_SINGLE_SHOT)
         read_time_ms = millis();
     }
 }
--- examples/sts/usage.cpp
+++ examples/sts/usage.cpp
@@ -2,59 +2,60 @@
 #include "Sts3x.h"
 
 #define USE_SINGLE_SHOT
-//#define USE_PERIODIC_DATA
 
-SerialLogHandler logHandler(115200, LOG_LEVEL_ALL, {
+SerialLogHandler logHandler(
+  115200,
+  LOG_LEVEL_ALL,
+  {
     // { "app", LOG_LEVEL_ALL },
     // { "hal", LOG_LEVEL_NONE},
     // { "lwip", LOG_LEVEL_NONE},
-});
-
-STARTUP(
-    System.enableFeature(FEATURE_RESET_INFO);
-    System.enableFeature(FEATURE_RETAINED_MEMORY);
+  }
 );
+
+STARTUP(System.enableFeature(FEATURE_RESET_INFO);
+        System.enableFeature(FEATURE_RETAINED_MEMORY););
 
 SYSTEM_THREAD(ENABLED);
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
-Sts3x* sts;
+Sts3x sts(Wire, Sts3x::ADDR_A, 255);
 
-void setup() {
+void setup()
+{
     Wire.setSpeed(CLOCK_SPEED_100KHZ);
-    sts  = new (std::nothrow) Sts3x(Wire, Sts3x::Addr::ADDR_A, 0);
-    sts->init();
+    Wire.begin();
+    if (!sts.init()) {
+        Log.error("failed to initialize device, halting");
+        while (true) {}
+    }
 
-#if defined(USE_PERIODIC_DATA)
-    sts->measure(Sts3x::Mode::PERIODIC_DATA, Sts3x::SingleMode::SINGLE_NONE, Sts3x::PeriodicMode::HIGH_10_MPS);
-#endif//defined(USE_PERIODIC_DATA)
+#if !defined(USE_SINGLE_SHOT)
+    sts.startPeriodicMeasurement(Sts3x::PeriodicMode::HIGH_10_MPS);
+#endif // !defined(USE_SINGLE_SHOT)
 }
 
-void loop() {
+void loop()
+{
     static uint32_t read_time_ms = millis();
-    
-    if(millis() - read_time_ms >= 1000) {
+
+    if (millis() - read_time_ms >= 1000) {
+        float temp;
 #if defined(USE_SINGLE_SHOT)
-        float temp = 0;
-        if(sts->singleShotMeasureAndRead(temp, Sts3x::SingleMode::LOW_NO_CLOCK_STRETCH) == 
-                SensirionBase::ErrorCodes::NO_ERROR) {
-            Log.info("temp:%f", temp);
-        }
-        else {
-            Log.info("failed to read temp");
+        if (sts.singleMeasurement(
+              temp, Sts3x::SingleMode::LOW_NO_CLOCK_STRETCH
+            )) {
+            Log.info("temp: %f", temp);
+        } else {
+            Log.error("failed to read temp");
         }
 #else
-        Vector<float> values {};
-        if(sts->periodicDataRead(values) == 
-                SensirionBase::ErrorCodes::NO_ERROR) {
-            for(auto iter : values) {
-                Log.info("values:%f", iter);
-            }
+        if (sts.periodicDataRead(temp)) {
+            Log.info("values: %f", temp);
+        } else {
+            Log.error("failed to read periodic temp");
         }
-        else {
-            Log.info("failed to read periodic temp");
-        }
-#endif //defined(USE_SINGLE_SHOT)
+#endif // defined(USE_SINGLE_SHOT)
         read_time_ms = millis();
     }
 }
